3dad0765949e0c1cd7804b95bf7747ebff88aa88
Omar Yasser <omaryasser011549@gmail.com>
2025-08-14 20:43:20 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
                 hostname: "images.unsplash.com",
                 port: "",
                 pathname: "/**",
-            }و
+            },
             {
         protocol: "https",
         hostname: "picsum.photos",
